782d20adab12969c19c4b2af74d1722e7f449256
Сергей Дубовой <follow@MacBook-Air-Sergej.local>
2021-11-16 15:07:32 +1000

    init commit

---
+++ src/authentication/views.py
@@ -0,0 +1,13 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth import get_user_model # If used custom user model
+
+from .serializers import UserSerializer
+
+
+class RegisterUser(CreateAPIView):
+    model = get_user_model()
+    permission_classes = [
+        AllowAny 
+    ]
+    serializer_class = UserSerializer
