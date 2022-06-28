ee10672aeb0498349c2e4ede460475b21a9e2a43
follow <follow12587@gmail.com>
2022-06-28 19:06:39 +1000

    little roll back

--- src/api/views.py
+++ src/api/views.py
@@ -29,30 +29,30 @@ class SettingsView(ReadOnlyModelViewSet):
         queryset = self.get_queryset()
         queryset: QuerySet
 
-        isAppsflyerActive = queryset.filter(name="isAppsflyerActive").first()
-        appsFlyerCounterMax = queryset.filter(
-            name="appsFlyerCounterMax").first()
-        appsFlyerCounter = queryset.filter(name="appsFlyerCounter").first()
+        # isAppsflyerActive = queryset.filter(name="isAppsflyerActive").first()
+        # appsFlyerCounterMax = queryset.filter(
+        #     name="appsFlyerCounterMax").first()
+        # appsFlyerCounter = queryset.filter(name="appsFlyerCounter").first()
         queryset = queryset.exclude(
             name__in=["appsFlyerCounterMax", "appsFlyerCounter"])
 
-        if not isAppsflyerActive or not appsFlyerCounter or not appsFlyerCounterMax:
-            serializer = self.serializer_class(queryset, many=True)
-            return Response(serializer.data)
+        # if not isAppsflyerActive or not appsFlyerCounter or not appsFlyerCounterMax:
+        #     serializer = self.serializer_class(queryset, many=True)
+        #     return Response(serializer.data)
 
-        try:
-            appsFlyerCounter.value = str(int(appsFlyerCounter.value) + 1)
-            if int(appsFlyerCounter.value) >= int(appsFlyerCounterMax.value):
-                appsFlyerCounter.value = '0'
-                isAppsflyerActive.value = '1'
-            else:
-                isAppsflyerActive.value = '0'
-        except ValueError:
-            serializer = self.serializer_class(queryset, many=True)
-            return Response(serializer.data)
+        # try:
+        #     appsFlyerCounter.value = str(int(appsFlyerCounter.value) + 1)
+        #     if int(appsFlyerCounter.value) >= int(appsFlyerCounterMax.value):
+        #         appsFlyerCounter.value = '0'
+        #         isAppsflyerActive.value = '1'
+        #     else:
+        #         isAppsflyerActive.value = '0'
+        # except ValueError:
+        #     serializer = self.serializer_class(queryset, many=True)
+        #     return Response(serializer.data)
 
-        isAppsflyerActive.save()
-        appsFlyerCounter.save()
+        # isAppsflyerActive.save()
+        # appsFlyerCounter.save()
 
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
