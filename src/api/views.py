5ca09407074c9570949334c41a930f70ee97ad29
follow <follow12587@gmail.com>
2022-06-28 14:26:36 +1000

    appsFlyer counter activation

--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from django.db.models.query import QuerySet
 
 from . import serializers, models
 
@@ -23,9 +25,42 @@ class SettingsView(ReadOnlyModelViewSet):
     serializer_class = serializers.SettingsSerializer
     permission_classes = [AllowAny, ]
 
+    def list(self, request):
+        queryset = self.get_queryset()
+        queryset: QuerySet
+
+        isAppsflyerActive = queryset.filter(name="isAppsflyerActive").first()
+        appsFlyerCounterMax = queryset.filter(
+            name="appsFlyerCounterMax").first()
+        appsFlyerCounter = queryset.filter(name="appsFlyerCounter").first()
+        queryset = queryset.exclude(
+            name__in=["appsFlyerCounterMax", "appsFlyerCounter"])
+
+        if not isAppsflyerActive or not appsFlyerCounter or not appsFlyerCounterMax:
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data)
+
+        try:
+            appsFlyerCounter.value = str(int(appsFlyerCounter.value) + 1)
+            if int(appsFlyerCounter.value) >= int(appsFlyerCounterMax.value):
+                appsFlyerCounter.value = '0'
+                isAppsflyerActive.value = '1'
+            else:
+                isAppsflyerActive.value = '0'
+        except ValueError:
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data)
+
+        isAppsflyerActive.save()
+        appsFlyerCounter.save()
+
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
 
 class SettingsMexicaView(ReadOnlyModelViewSet):
-    queryset = models.Settings.objects.filter(app__name='Préstamos Personales').all()
+    queryset = models.Settings.objects.filter(
+        app__name='Préstamos Personales').all()
     serializer_class = serializers.SettingsSerializer
     permission_classes = [AllowAny, ]
 
